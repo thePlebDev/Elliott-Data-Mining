280d0974d2a0223dfaa5374c9d64672fd852f0a8
Tristan Elliott <>
2022-07-07 19:26:36 -0300

    finished CRUD functionality

--- src/main/java/com/example/EDMWebsite/Services/UserService.java
+++ src/main/java/com/example/EDMWebsite/Services/UserService.java
@@ -2,8 +2,10 @@ package com.example.EDMWebsite.Services;
 
 import com.example.EDMWebsite.Models.Calf;
 import com.example.EDMWebsite.Models.User;
+import com.example.EDMWebsite.Repositories.CalfRepository;
 import com.example.EDMWebsite.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,11 +17,13 @@ public class UserService {
 
     private UserRepository userRepository;
     private BCryptPasswordEncoder passwordEncoder;
+    private CalfRepository calfRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder,CalfRepository calfRepository){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.calfRepository = calfRepository;
     }
 
     public User saveUser(User user){
@@ -41,4 +45,23 @@ public class UserService {
         userRepository.save(userFound);
         return calf.getTagNumber();
     }
+    //TODO: HANDLE ALL OPTIONAL ERROR
+    //TODO: NEED TO PROTECT AGAINST RANDOM DELETIONS
+    public String deleteCalfById(Long calfId, Authentication auth){
+        String username = auth.getName();
+
+        User foundUser = this.userRepository.findByUsername(username).get();
+        Calf foundCalf = this.calfRepository.findById(calfId).get();
+       // foundUser.getCalves().stream().filter(calf -> calf.getId() == calfId)
+
+        //foundUser.removeCalf(foundCalf);
+        this.calfRepository.delete(foundCalf);
+
+       // this.userRepository.save(foundUser);
+
+
+        return "THIS NEEDS TO BE UPDATED";
+    }
+
+
 }
