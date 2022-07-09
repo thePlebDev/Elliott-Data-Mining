21094a320513f6b10e98b58924b43b2b633cffdb
Tristan Elliott <>
2022-07-08 22:03:22 -0300

    trying to figure out how to handle exceptions

--- src/main/java/com/example/EDMWebsite/Controllers/CalfController.java
+++ src/main/java/com/example/EDMWebsite/Controllers/CalfController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -30,6 +27,7 @@ public class CalfController {
         this.userService = userService;
     }
 
+    @ExceptionHandler
     @GetMapping("/add")
     public String addingCalf(Model model){
         Calf calf = new Calf();
